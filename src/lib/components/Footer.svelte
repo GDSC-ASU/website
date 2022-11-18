19c0d3bc322d9f294c7d47fad151aefb12172c37
Baraa Al-Masri <m.baraa.masri@gmail.com>
2022-11-18 19:43:51 +0300

    ...

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,96 +1,81 @@
 <script lang="ts">
-    import Separator from "./Separator.svelte";
+  import Separator from "./Separator.svelte";
+  import PhotoLink from "$lib/ui/PhotoLink.svelte";
 </script>
 
 <div class="md:w-auto mx-[35px] md:mx-[55px] xl:mx-[145px] my-[70px]">
-    <Separator />
-    <div class="hidden md:block mt-[70px]">
-        <div class="flex justify-between">
-            <div>
-                <img
-                    class=""
-                    width={420}
-                    height={65}
-                    alt="GDSC - ASU"
-                    src="/images/gdsc-logo.png"
-                />
-            </div>
-            <div class="text-grey-1">
-                <h2 class="text-[25px] text-grey-2 ">Follow Us:</h2>
-                <a
-                    class="pr-[10px]"
-                    target="_blank"
-                    href="https://facebook.com/GDSCASU"
-                    rel="noreferrer">Facebook</a
-                >
-                <a
-                    class="pr-[10px]"
-                    target="_blank"
-                    href="https://instagram.com/gdscasu"
-                    rel="noreferrer">Instagram</a
-                >
-                <a
-                    class="pr-[10px]"
-                    target="_blank"
-                    href="https://linkedin.com/company/gdscasu"
-                    rel="noreferrer">LinkedIn</a
-                >
-                <a
-                    class="pr-[10px]"
-                    target="_blank"
-                    href="https://github.com/GDSC-ASU"
-                    rel="noreferrer">GitHub</a
-                >
-                <a
-                    class="pr-[10px]"
-                    target="_blank"
-                    href="https://gdsc.community.dev/applied-science-private-university/"
-                    rel="noreferrer">GDSC Platform</a
-                >
-            </div>
-        </div>
-    </div>
-
-    <div class="block md:hidden mt-[35px]">
+  <Separator />
+  <div class="hidden md:block mt-[70px]">
+    <div class="flex justify-between">
+      <div>
         <img
-            class=""
-            width={290}
-            height={45}
-            alt="GDSC - ASU"
-            src="/images/gdsc-logo.png"
+          class=""
+          width={420}
+          height={65}
+          alt="GDSC - ASU"
+          src="/images/gdsc-logo.png"
         />
-        <div class="text-grey-1 mt-[35px]">
-            <h2 class="text-[25px] text-grey-2 ">Follow Us:</h2>
-            <a
-                class="pr-[10px]"
-                target="_blank"
-                href="https://facebook.com/GDSCASU"
-                rel="noreferrer">Facebook</a
-            >
-            <a
-                class="pr-[10px]"
-                target="_blank"
-                href="https://instagram.com/gdscasu"
-                rel="noreferrer">Instagram</a
-            >
-            <a
-                class="pr-[10px]"
-                target="_blank"
-                href="https://linkedin.com/company/gdscasu"
-                rel="noreferrer">LinkedIn</a
-            >
-            <a
-                class="pr-[10px]"
-                target="_blank"
-                href="https://github.com/GDSC-ASU"
-                rel="noreferrer">GitHub</a
-            >
-            <a
-                class="pr-[10px]"
-                target="_blank"
-                href="https://gdsc.community.dev/applied-science-private-university/"
-                rel="noreferrer">GDSC Platform</a
-            >
+      </div>
+      <div class="text-grey-1">
+        <h2 class="text-[25px] text-grey-2 w-[150px] ">Follow Us:</h2>
+        <div>
+          <PhotoLink
+            link="https://www.facebook.com/GDSCASU"
+            imagePath="/logos/facebook.png"
+          />
+          <PhotoLink
+            link="https://linkedin.com/company/gdscasu"
+            imagePath="/logos/instagram.png"
+          />
+          <PhotoLink
+            link="https://www.linkedin.com/company/GDSCASU"
+            imagePath="/logos/linkedin.png"
+          />
+          <PhotoLink
+            link="https://github.com/GDSC-ASU"
+            imagePath="/logos/github.png"
+          />
+          <PhotoLink
+            link="https://gdsc.community.dev/applied-science-private-university/"
+            imagePath="/logos/google.png"
+          />
         </div>
+      </div>
     </div>
+  </div>
+
+  <div class="block md:hidden mt-[35px]">
+    <img
+      class=""
+      width={290}
+      height={45}
+      alt="GDSC - ASU"
+      src="/images/gdsc-logo.png"
+    />
+    <div class="text-grey-1 pt-[40px]">
+      <h2 class="text-[25px] text-grey-2 w-[150px] ">Follow Us:</h2>
+      <div>
+        <PhotoLink
+          link="https://www.facebook.com/GDSCASU"
+          imagePath="/logos/facebook.png"
+        />
+        <PhotoLink
+          link="https://linkedin.com/company/gdscasu"
+          imagePath="/logos/instagram.png"
+        />
+        <PhotoLink
+          link="https://www.linkedin.com/company/GDSCASU"
+          imagePath="/logos/linkedin.png"
+        />
+        <PhotoLink
+          link="https://github.com/GDSC-ASU"
+          imagePath="/logos/github.png"
+        />
+        <PhotoLink
+          link="https://gdsc.community.dev/applied-science-private-university/"
+          imagePath="/logos/google.png"
+        />
+      </div>
+    </div>
+  </div>
 </div>
---
+++ src/lib/ui/PhotoLink.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let link: string;
+  export let imagePath: string;
+</script>
+
+<a href={link} target="_blank">
+  <img
+    alt={link}
+    src={imagePath}
+    class="w-[25px] h-[25px] inline mt-[15px] mr-[20px]"
+  />
+</a>
