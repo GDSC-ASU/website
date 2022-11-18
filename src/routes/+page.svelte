122bf9d93529579f5c148d179c758c089223a162
Baraa Al-Masri <m.baraa.masri@gmail.com>
2022-11-18 19:50:07 +0300

    added follow us in header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,37 @@
 <script lang="ts">
-    import Events from "$lib/components/Events.svelte";
-    import FAQs from "$lib/components/FAQs.svelte";
-    import Footer from "$lib/components/Footer.svelte";
-    import { default as Header } from "$lib/components/Header/Main.svelte";
+  import Events from "$lib/components/Events.svelte";
+  import FAQs from "$lib/components/FAQs.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import { default as Header } from "$lib/components/Header/Main.svelte";
 
-    import Intro from "$lib/components/Intro.svelte";
-    import Projects from "$lib/components/Projects.svelte";
-    import Team from "$lib/components/Team.svelte";
-    import SectionWrapper from "$lib/ui/SectionWrapper.svelte";
+  import Intro from "$lib/components/Intro.svelte";
+  import Projects from "$lib/components/Projects.svelte";
+  import Team from "$lib/components/Team.svelte";
+  import SectionWrapper from "$lib/ui/SectionWrapper.svelte";
 </script>
 
 <svelte:head>
-    <title>GDSC - ASU</title>
+  <title>GDSC - ASU</title>
 </svelte:head>
 
 <div class="">
-    <Header />
-    <SectionWrapper id="intro">
-        <Intro />
-    </SectionWrapper>
-    <SectionWrapper id="projects">
-        <Projects />
-    </SectionWrapper>
-    <SectionWrapper id="events">
-        <Events />
-    </SectionWrapper>
-    <SectionWrapper id="team">
-        <Team />
-    </SectionWrapper>
-    <SectionWrapper id="faqs">
-        <FAQs />
-    </SectionWrapper>
+  <Header />
+  <SectionWrapper id="intro">
+    <Intro />
+  </SectionWrapper>
+  <SectionWrapper id="projects">
+    <Projects />
+  </SectionWrapper>
+  <SectionWrapper id="events">
+    <Events />
+  </SectionWrapper>
+  <SectionWrapper id="team">
+    <Team />
+  </SectionWrapper>
+  <SectionWrapper id="faqs">
+    <FAQs />
+  </SectionWrapper>
+  <SectionWrapper id="footer">
     <Footer />
+  </SectionWrapper>
 </div>
