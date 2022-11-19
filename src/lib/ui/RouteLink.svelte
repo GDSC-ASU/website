e7e89ca3154aba8156addb73f32924e61148e21d
Laith Alqaisi <32362046+Laith-Alqaisi@users.noreply.github.com>
2022-11-19 13:22:44 +0300

    Added coming soon to routelink

--- src/lib/ui/RouteLink.svelte
+++ src/lib/ui/RouteLink.svelte
@@ -2,20 +2,26 @@
     import { page } from "$app/stores";
 
     import { onMount } from "svelte";
+    import ComingSoon from "./ComingSoon.svelte";
     export let title: string;
     export let href = "#";
     export let _class: string = "";
 
     let isActive = false;
-
+    export let isComingSoon = false;
     onMount(() => {
         isActive =
             $page.url.pathname.includes(href) || $page.url.hash.includes(href);
     });
 </script>
 
-<a {href} class:active={isActive} class={`${_class} hover:text-[#444]`}>
-    {title}
+<a {href} class:active={isActive} class={`${_class} hover:text-[#444] text-center md:inline-block`}>
+    <div class="grid align-items-center">
+        {title}
+        {#if isComingSoon}
+        <ComingSoon/>
+        {/if}
+    </div>
 </a>
 
 <style>
