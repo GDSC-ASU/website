2799debaaf56b12e820a381c4c2ad384c98aa4eb
Baraa Al-Masri <m.baraa.masri@gmail.com>
2023-04-01 22:27:12 +0300

    fixed FAQ sizes

--- src/lib/components/FAQs.svelte
+++ src/lib/components/FAQs.svelte
@@ -13,13 +13,13 @@
   <div class="w-[100%] my-[20px] ">
     <div class="w-[100%] grid place-items-center py-[40px] ">
       <div
-        class="w-[338.6px] h-[100px] md:w-[846.6px] md:h-[250px] xl:w-[1354.5px] xl:h-[400px]  bg-[url('/images/faqs.svg')] bg-contain bg-no-repeat bg-center grid place-items-center "
+        class="w-full h-auto md:w-[846.6px] md:h-[250px] xl:w-[1354.5px] xl:h-[400px]  bg-[url('/images/faqs.svg')] bg-contain bg-no-repeat bg-center grid place-items-center "
       >
         <div class="text-center pb-[20px]">
           <h1 class="text-[25px] md:text-[40px] font-bold text-grey-2 ">
             F.A.Q.
           </h1>
-          <h2 class="text-[10px] md:text-[22px] xl:text-[27px] text-grey-1 ">
+          <h2 class="text-[11px] md:text-[22px] xl:text-[27px] text-grey-1 ">
             Frequent Asked Questions about <Google
               >&nbsp;Developer Student Clubs</Google
             >
@@ -28,7 +28,7 @@
       </div>
     </div>
     <div class="w-[100%]">
-      <div class="flex flex-wrap justify-center px-[35px]">
+      <div class="grid grid-cols-1 px-[35px]">
         {#each faqs as faq}
           <FAQV {faq} />
         {/each}
