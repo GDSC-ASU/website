76c89c90db0472c7e43f0431752f7d6005abe517
Ahmad Mahmoud <abuhamdiehahmad@gmail.com>
2023-11-27 14:55:02 +0300

    added fixed height and width

--- src/lib/components/FAQ.svelte
+++ src/lib/components/FAQ.svelte
@@ -5,8 +5,10 @@
 </script>
 
 {#if faq}
-    <div class="text-center m-[20px] ">
-        <h1 class="text-[20px] md:text-[35px] text-grey-2 font-bold">{faq.question}&quest;</h1>
+    <div class="text-center m-[20px]">
+        <h1 class="text-[20px] md:text-[35px] text-grey-2 font-bold">
+            {faq.question}&quest;
+        </h1>
         <h2 class="text-[15px] md:text-[20px] text-grey-1">{faq.answer}</h2>
     </div>
 {/if}
--- src/lib/components/Header/Desktop.svelte
+++ src/lib/components/Header/Desktop.svelte
@@ -1,35 +1,47 @@
 <script lang="ts">
-  import { page } from "$app/stores";
+    import { page } from "$app/stores";
 
-  import RouteLink from "$lib/ui/RouteLink.svelte";
-  import Separator from "../Separator.svelte";
+    import RouteLink from "$lib/ui/RouteLink.svelte";
+    import Separator from "../Separator.svelte";
 </script>
 
 <div
-  class="flex justify-between pb-[15px] pt-[25px] mb-[10px] mx-[30px] md:mx-[45px] xl:mx-[145px]"
+    class="flex justify-between pb-[15px] pt-[25px] mb-[10px] mx-[30px] md:mx-[45px] xl:mx-[145px]"
 >
-  <div class="">
-    <img
-      class="block"
-      width={420}
-      height={65}
-      alt="GDSC - ASU"
-      src="/images/gdsc-logo.png"
-    />
-  </div>
-  <div class="">
-    {#key [$page.url.hash, $page.url.pathname]}
-      <div class="font-[400] uppercase pt-[24px] px-[7px]">
-        <nav>
-          <RouteLink title="Events" href="#events" _class="pr-[25px]" />
-          <RouteLink title="Our Team" href="#team" _class="pr-[25px]" />
-          <RouteLink title="FAQs" href="#faqs" _class="pr-[25px]" />
-          <RouteLink title="Follow US" href="#footer" _class="lg:pr-[25px]" />
-        </nav>
-      </div>
-    {/key}
-  </div>
+    <div class="">
+        <img
+            class="block w-[420px] h-[65px]"
+            width={420}
+            height={65}
+            alt="GDSC - ASU"
+            src="/images/gdsc-logo.png"
+        />
+    </div>
+    <div class="">
+        {#key [$page.url.hash, $page.url.pathname]}
+            <div class="font-[400] uppercase pt-[24px] px-[7px]">
+                <nav>
+                    <RouteLink
+                        title="Events"
+                        href="#events"
+                        _class="pr-[25px]"
+                    />
+                    <RouteLink
+                        title="Our Team"
+                        href="#team"
+                        _class="pr-[25px]"
+                    />
+                    <RouteLink title="FAQs" href="#faqs" _class="pr-[25px]" />
+                    <RouteLink
+                        title="Follow US"
+                        href="#footer"
+                        _class="lg:pr-[25px]"
+                    />
+                </nav>
+            </div>
+        {/key}
+    </div>
 </div>
 <div class="mx-[30px] md:mx-[45px] xl:mx-[145px]">
-  <Separator />
+    <Separator />
 </div>
