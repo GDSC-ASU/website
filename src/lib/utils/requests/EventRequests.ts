b7b0cf56fdb447fbe7a20f6f5f1e2ad8cf24f88c
RemasMehyar <remas_xl@hotmail.com>
2024-03-02 22:25:37 +0300

    cleaned

--- src/lib/utils/requests/EventRequests.ts
+++ src/lib/utils/requests/EventRequests.ts
@@ -1,32 +1,28 @@
 import { Event } from "$lib/data/Event";
+
 export default class EventRequest {
 	public static async getAllEvents() {
-	  const events = await Event.findAll();
-	  return events.map((event) => event.toJSON());
+		const events = await Event.findAll();
+		return events.map((event) => event.toJSON());
 	}
-	public static async createEvent(name: string, description:string, link: string, imagePath: string, altImagePath: string) {
-  const newEvent = await Event.create({
-	name,
-	description,
-	link,
-	imagePath,
-	altImagePath
-});
+	public static async createEvent(body: any) {
+		const event = await Event.create(body);
+		return event;
 	}
-public static async updateEvent(id: string,name: string, description:string, link: string, imagePath: string, altImagePath: string){
-	const events = await Event.findByPk(id); 
-	await events.update({
-		name,
-		description,
-		link,
-		imagePath,
-		altImagePath
-	});
-	return events.toJSON();
+	public static async updateEvent(body: any) {
+		const event = await Event.findByPk(body.id);
+		if (!event) {
+			throw new Error("Event not found");
+		}
+		await event.update(body);
+		return event.toJSON();
+	}
+	public static async deleteEvent(body: any) {
+		const event = await Event.findByPk(body.id);
+		if (!event) {
+			throw new Error("Event not found");
+		}
+		await event.destroy();
+		return { message: "Deleted" };
+	}
 }
-public static async deleteEvent(id: string)
-{
-	const events = await Event.findByPk(id);
-	await events.destroy();
-	return{message:'Deleted'}
-}}
