33960f5d3b0153eaaa6d2d862bd470b78032e9a3
Baraa Al-Masri <pub@mbaraa.com>
2024-03-05 14:03:37 +0300

    Add sync DB function

--- src/lib/data/gdscDB.ts
+++ src/lib/data/gdscDB.ts
@@ -1,20 +1,25 @@
 import { Sequelize } from "sequelize-typescript";
-import { DataType } from "sequelize-typescript";
+import { Event } from "$lib/data/Event";
 const sqliteFilePath = import.meta.env.VITE_SQLITE_FILE_PATH;
 
 export const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: sqliteFilePath,
+	dialect: "sqlite",
+	storage: sqliteFilePath,
 });
 async function testConnection() {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+	try {
+		await sequelize.authenticate();
+		console.log("Connection has been established successfully.");
+	} catch (error) {
+		console.error("Unable to connect to the database:", error);
+	}
 }
+
 testConnection();
-sequelize.sync({});
+
+export function syncDatabase() {
+	sequelize.sync();
+	Event.sync();
+}
 
 export default sequelize;
