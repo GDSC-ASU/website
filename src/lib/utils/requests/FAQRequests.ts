c359bddb6215e877fed2ba66fc2038319c551bbb
Dimah <dimahmehdawi@gmail.com>
2024-03-25 23:32:59 +0300

    Update FAQ requests

--- src/lib/utils/requests/FAQRequests.ts
+++ src/lib/utils/requests/FAQRequests.ts
@@ -1,22 +1,17 @@
 import type FAQ from "$lib/entities/FAQ";
+import Requests from "./Requests";
 
-const faqs = [
-	{
-		question: "Question 1",
-		answer: "Answer 1",
-	},
-	{
-		question: "Question 2",
-		answer: "Answer 2",
-	},
-	{
-		question: "Question 3",
-		answer: "Answer 3",
-	},
-];
-
-export default class FAQRequests {
-	public static async getFAQs(): Promise<FAQ[]> {
-		return faqs;
+export default class FAQRequest {
+	public static async getAllFAQs(): Promise<FAQ[]> {
+		return await Requests.makeRequest("GET", "faq", null)
+			.then((resp) => resp.json())
+			.then((projects) => projects.map((e: FAQ) => e as FAQ))
+			.catch((err) => {
+				console.log(err);
+				return [];
+			});
 	}
+	public static async createFAQ(body: any) {}
+	public static async updateFAQ(body: any) {}
+	public static async deleteFAQ(body: any) {}
 }
